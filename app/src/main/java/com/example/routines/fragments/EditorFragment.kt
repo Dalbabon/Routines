0b48cf125de74c675774ddc48ed6783962d9a8fc
Dalbabon <gusarovpol777@mail.ru>
2025-05-16 15:47:37 +0400

    Editor fragment

--- app/src/main/java/com/example/routines/fragments/EditorFragment.kt
+++ app/src/main/java/com/example/routines/fragments/EditorFragment.kt
@@ -1,21 +1,53 @@
 package com.example.routines.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.routines.R
+import com.example.routines.databinding.FragmentEditorBinding
 
 
 class EditorFragment : Fragment() {
+
+    private var _binding: FragmentEditorBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editor, container, false)
+        _binding = FragmentEditorBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        switchView()
+
+    }
+
+    private fun switchView() {
+        binding.switch1.setOnCheckedChangeListener{
+                _, isChecked ->
+            // isChecked - новое состояние переключателя
+            if (isChecked) {
+                // Действия при включении
+                binding.textView7.setTextColor(Color.argb(255, 255, 177, 23))
+                binding.textView6.setTextColor(Color.argb(255, 69, 77, 91))
+            } else {
+                // Действия при выключении
+
+                binding.textView6.setTextColor(Color.argb(255, 255, 177, 23))
+                binding.textView7.setTextColor(Color.argb(255, 69, 77, 91))
+            }
+        }
+    }
+
     companion object {
 
         @JvmStatic
