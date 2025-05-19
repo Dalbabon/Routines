d40dd72ebb73ee0a46439a92646b4bdc778db230
ilshat <ilshat_shamsuvarov@mail.ru>
2025-05-20 01:01:28 +0400

    Доделал интерфес для activity_reminder.xml, но при вызове клавиатуры она загараживает эти поля. 40 минут пытался починить. Не получилось(

--- app/src/main/java/com/example/routines/ReminderActivity.kt
+++ app/src/main/java/com/example/routines/ReminderActivity.kt
@@ -2,7 +2,11 @@ package com.example.routines
 
 import android.nfc.Tag
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +16,22 @@ import com.example.routines.fragments.EditorFragment
 
 class ReminderActivity : AppCompatActivity() {
     lateinit var binding: ActivityReminderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        val minuteEditText: EditText = findViewById(R.id.editTextMinute)
+//        minuteEditText.doAfterTextChanged {
+//            val minutes = it.toString().toIntOrNull()
+//
+//            if (minutes == null || minutes !in 0..59) {
+//                minuteEditText.error = "Минуты от 0 до 59"
+//            } else {
+//                // Всё ок — можно использовать значение
+//                Log.d("TIME_INPUT", "Минуты: $minutes")
+//            }
+//        }
+
         binding = ActivityReminderBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
@@ -25,10 +43,11 @@ class ReminderActivity : AppCompatActivity() {
         onClickBack()
     }
 
-    private fun onClickBack(){
+
+    private fun onClickBack() {
         binding.buttonBack.setOnClickListener {
             finish()
-            Log.d("BTN_BACK","Кнопка Назад")
+            Log.d("BTN_BACK", "Кнопка Назад")
         }
     }
 
